ecafb51ee4a7c4f1d84c5d89eb1f6d5ea3a38393
BaradRonaksinh <baradronaksinh21303@gmail.com>
2024-12-22 23:02:21 +0530

    create a  footer component and making a dynamic component for product data

---
+++ src/Components/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import '../css/Footer.css'
+import { FaSquareXTwitter ,FaMeta, FaLinkedin,FaYoutube  } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <>
+    <div className='footer'>
+
+      <div className='fullSet'>
+        <div className='set-1'>
+            <div className='ch'>
+                <a href="">About</a>
+                <a href="">Home</a>
+                <a href="">Shop</a>
+                <a href="">Our Story</a>
+                <a href="">Blogs</a>
+            </div>
+            <div className='ch'>
+                <a href="">Help</a>
+                <a href="">Shipping & Reaturns</a>
+                <a href="">Track Order</a>
+                <a href="">FAQs</a>
+            </div>
+            <div className='ch'>
+                <a href="">Contact</a>
+                <a href="">Phone:</a>
+                <a href="">Email:</a>
+            </div>
+        </div>
+        <div className='set-2'>
+            <h1>Receive new promotions</h1>
+            <p>Lorem ipsum dolor sit.</p>
+            <div className='eml'>
+            <input type="email"  placeholder='Input your email' />
+            <button>Subscribe</button>
+            </div>
+            <div className='social'>
+                <a href=""><FaSquareXTwitter /></a>
+                <a href=""><FaMeta /></a>
+                <a href=""><FaLinkedin /></a>
+                <a href=""><FaYoutube /></a>
+            </div>
+            <div>
+                <a href="">&copy; 2024 Ronak,</a>
+                <a href="">Privacy</a>
+                <a href="">Terms</a>
+                <a href="">Sitemap</a>
+            </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import '../css/Home.css'
-import main_img from "../../public/images/main_jpg.jpg"
+import firstBgImg from '../assets/images/bg_p1.jpg'
+import Footer from '../Components/Footer'
 
 
 const Home = () => {
+
+  const imgStyle = {
+    backgroundImage:`url(${firstBgImg})`,
+    backgroundSize:'cover',
+    backgroundPosition: 'center',
+    height: '90vh',
+    width: '100%',
+    // opacity: 1,
+  };
+
   return (
     <>
     <Navbar />
-      <div className='bg_img' style={ba} >
-
+      <div className='large-bg-img' style={imgStyle}>
+        <div className='first-st'>
+          <h1 className='main-header'>Gift for your skin</h1>
+          <p className='per'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Imernatur omnis, rerum a id.</p>
+          <button className='b1'>Shop Now</button>
+        </div>
       </div>
+
+
+      <Footer />
     </>
   )
 }
